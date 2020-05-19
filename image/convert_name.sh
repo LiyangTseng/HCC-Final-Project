b0fa7ebc35f9b29a507ae8a15b629cd137b0d98e
Justin <justin900429.gmail.com>
2020-05-19 21:04:08 +0800

    Finish image rename

--- image/convert_name.sh
+++ image/convert_name.sh
@@ -4,8 +4,8 @@ AMOUNT_FILES="$(ls -1 | wc -l)"
 BASE_NAME="$(basename "$PWD" | tr '[:upper:]' '[:lower:]')"
 COUNT=1
 
-for filename in ./*.xml
+for filename in ./*.jpg
 do
-    mv ${filename} "${BASE_NAME}_${COUNT}.xml"
+    mv ${filename} "${BASE_NAME}_${COUNT}.jpg"
     COUNT=$((COUNT+1))
 done
